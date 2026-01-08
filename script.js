8d310976a5d3d028e819602cf35847d31e09044e
Shivam Sayankar <111188660+Shivam-Sayankar@users.noreply.github.com>
2026-01-09 01:32:56 +0530

    Setup basic HTML structure and add basic functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hk")
+// console.log("hk")
+
+let library = [];
+
+
+// Book constructor
+function Book(title, author, numOfPages, isRead, uniqueID) {
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.isRead = isRead;
+    this.uniqueID = uniqueID;
+}
+
+function addBookToLibrary(title, author, numOfPages, isRead) {
+
+    const newUniqueID = crypto.randomUUID();
+    const newBook = new Book(title, author, numOfPages, isRead, newUniqueID);
+
+    library.push(newBook);
+}
+
+const tempBook = new Book("temp potter", "jk lol", 104, true)
+console.log(tempBook)
