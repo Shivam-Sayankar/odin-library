dc6232e11e3d377a6b4b0afe75964c2fb25cd6b0
Shivam Sayankar <111188660+Shivam-Sayankar@users.noreply.github.com>
2026-01-11 16:39:43 +0530

    Add dialog element with form to create a new book

--- script.js
+++ script.js
@@ -50,3 +50,15 @@ function addBookToLibrary(title, author, numOfPages, isRead) {
 
 const tempBook = new Book("temp potter", "jk lol", 104, true)
 console.log(tempBook)
+
+const addBookButton = document.querySelector("#add-book-btn")
+const dialog = document.querySelector('dialog')
+const closeDialogButton = document.querySelector("#close-dialog-btn")
+
+addBookButton.addEventListener("click", (e) => {
+    dialog.showModal()
+})
+
+closeDialogButton.addEventListener("click", () => {
+    dialog.close()
+})
